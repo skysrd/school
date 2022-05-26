62b28f888b4fb55bd204aab8a966fa602268a31c
skysrd <37787079+skysrd@users.noreply.github.com>
2022-05-26 17:22:15 +0900

    Initial commit

---
+++ src/test/java/Skysrd/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package Skysrd.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
